cbebe89a383b58104d17f094475a8b36b739aaac
AmolikaJalkote <amolikajalkote61@gmail.com>
2017-08-25 15:44:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element=document.getElementById("main-text");
 //element.innerHTML='New Value';
 var img=document.getElementById('myimg');
 img.onclick=function(){
-    img.style.marginleft='100px';
+    img.style.marginLeft='100px';
 };
