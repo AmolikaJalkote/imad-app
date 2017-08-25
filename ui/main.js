655f61d014e451d0b4c48798fba2df077db981fd
AmolikaJalkote <amolikajalkote61@gmail.com>
2017-08-25 15:52:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img=document.getElementById('myimg');
 var marginLeft=0;
 function moveRight(){
   marginLeft=marginLeft + 10 ;
-  img.style.marginLeft + 'px'
-};
+  img.style.marginLeft + 'px';
+}
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
     //img.style.marginLeft='100px';
