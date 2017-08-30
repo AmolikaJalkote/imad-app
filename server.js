d4f6b41ab2750fc068e19841c5b0c7bc91ea91f1
AmolikaJalkote <amolikajalkote61@gmail.com>
2017-08-30 23:28:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,8 @@ app.get('/ui/main.js', function (req, res) {
 var names=[];
 app.get('/submit/:name',function(req,res){
 //get name from request object
-    var name=req.params.name;
+    //var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     //JSON:Javascript Object Notation:Converts js object into strings.
     res.send(JSON.stringify(names));
