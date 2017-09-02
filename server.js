5efe5122ccda594289e676f04eea953de2780d00
AmolikaJalkote <amolikajalkote61@gmail.com>
2017-09-02 15:45:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+//--to connect to the database.
+var Pool=require(pg).Pool;
+
+var config={
+  user:'amolikajalkote',
+  database:'amolikajalkote',
+  host:'db.imad.hasura.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -119,9 +130,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+//--connecting the POOL
+var pool=new Pool(config);
+
 app.get('/test-db',function(req,res){
     //make select request
     //return a result with respose
+    pool.query('select* from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString())
+       } else{
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 app.get('/:articleName',function(req,res){
