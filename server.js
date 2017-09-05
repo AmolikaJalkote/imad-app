8d52eca5761af4e23e84cf436bd8ab5d6c23cee8
AmolikaJalkote <amolikajalkote61@gmail.com>
2017-09-05 18:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,13 @@ app.get('/test-db',function(req,res){
            res.status(500).send(err.toString())
        } else{
         //   res.send(JSON.stringify(result));
-           res.send(JSON.stringify(result.rows));
+          // res.send(JSON.stringify(result.rows));
+          if(result.rows.length==0){
+              res.status(404).send("Article not found");
+          }else{
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
        }
     });
 });
